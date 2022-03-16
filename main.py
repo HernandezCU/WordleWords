1d2b2f3fb8dc5b954b35a82dd80b706911c711e0
hernandezcu <hernandezcu@jbu.edu>
2022-03-16 10:27:27 -0500

    Added textboxes fixed errors added error handling added command Validation

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#Import the required Libraries
+# Import the required Libraries
+import tkinter
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 from tkinter import ttk
 from wordle_solver import *
-
 
 a_file = open("words.txt")
 file_contents = a_file.read()
@@ -14,105 +14,135 @@ words = file_contents.splitlines()
 def suggestions():
     count = {'a': 979, 'b': 281, 'c': 477, 'd': 393, 'e': 1233, 'f': 230, 'g': 311, 'h': 389, 'i': 671, 'j': 27,
              'k': 210, 'l': 719, 'm': 316, 'n': 575, 'o': 754, 'p': 367, 'q': 29, 'r': 899, 's': 669, 't': 729,
-             'u': 467, 'v': 153,'w': 195, 'x': 37, 'y': 425, 'z': 40}
+             'u': 467, 'v': 153, 'w': 195, 'x': 37, 'y': 425, 'z': 40}
 
     max = {'e': 1233, 'a': 979, 'r': 899, 'o': 754, 't': 729}
 
     min = {'j': 27, 'q': 29, 'x': 37, 'z': 40, 'v': 153}
 
-
-    label.configure(text="Most common: " + str(max))
-    label2.configure(text= "Least Common: "+ str(min))
-    st.insert('end', ';')
+    c = "Most common: " + str(max) + "\n" + "Least Common: " + str(min)
+    T.delete("1.0", "end")
+    T.insert(tkinter.END, c)
 
 
 def contains():
-   global words
-   letters = entry.get()
-   matches = []
-   count = 0
-   for word in words:
-     for letter in letters:
-         if letter in word:
-             count += 1
-     if count == len(letters):
-         matches.append(word)
-     count = 0
+    global words
+    letters = entry.get()
+    matches = []
+    count = 0
+    for word in words:
+        for letter in letters:
+            if letter in word:
+                count += 1
+        if count == len(letters):
+            matches.append(word)
+        count = 0
 
-   words = matches
-   print(words)
+    words = matches
+    print(words)
+    T.delete("1.0", "end")
+    T.insert(tkinter.END, words)
 
 
 def contains_at_index():
-   global words
-   data = entry.get().split(',')
-   letter = data[0]
-   index = int(data[1])
-   matches = []
-   for word in words:
-     if word[index] == letter:
-         matches.append(word)
+    global words
 
-   words = matches
-   print(words)
+    try:
+        data = entry.get().split(',')
+        letter = data[0]
+        index = int(data[1])
+        matches = []
+        try:
+            for word in words:
+                if word[index] == letter:
+                    matches.append(word)
+        except:
+            label.configure(text="Invalid Index (Out of Range)")
+        words = matches
+        print(words)
+        T.delete("1.0", "end")
+        T.insert(tkinter.END, words)
+
+    except:
+        label.configure(text="Invalid Command (letter, index)")
 
 
 def not_contains():
-   global words
-   letter = entry.get()
-   x = []
-   for word in words:
-     if letter not in word:
-         x.append(word)
-     else:
-         pass
-   print(x)
-   words = x
+    global words
+    try:
+        letter = entry.get()
+        x = []
+        for word in words:
+            if letter not in word:
+                x.append(word)
+            else:
+                pass
+        print(x)
+        words = x
+        T.delete("1.0", "end")
+        T.insert(tkinter.END, words)
+
+    except:
+        label.configure(text="Invalid Command")
 
 
 def not_contains_at_index():
-   global words
-   data = entry.get().split(',')
-   letter = data[0]
-   index = int(data[1])
-   x = []
-   for word in words:
-     if word[index] != letter:
-         x.append(word)
-     else:
-         pass
+    global words
+    try:
+        data = entry.get().split(',')
+        letter = data[0]
+        index = int(data[1])
+        x = []
 
-   words = x
-   print(words)
+        try:
+            for word in words:
+                if word[index] != letter:
+                    x.append(word)
+                else:
+                    pass
+        except:
+            label.configure(text="Invalid Index (Out of Range)")
+
+        words = x
+        print(words)
+        T.delete("1.0", "end")
+        T.insert(tkinter.END, words)
+
+    except:
+        label.configure(text="Invalid Command (letter, index)")
 
 
 def remove_word():
-   global words
-   word = entry.get()
-   words.remove(word)
-   print(words)
+    global words
+    try:
+        word = entry.get()
+        words.remove(word)
+        print(words)
+        T.delete("1.0", "end")
+        T.insert(tkinter.END, words)
 
+    except:
+        label.configure(text="Word not found!")
 
 def reset_words():
-   global words
-   a_file = open("words.txt")
-   file_contents = a_file.read()
-   words = file_contents.splitlines()
+    global words
+    a_file = open("words.txt")
+    file_contents = a_file.read()
+    words = file_contents.splitlines()
+    T.delete("1.0", "end")
+    T.insert(tkinter.END, words)
 
 
-win= Tk()
-win.geometry("750x500")
+win = Tk()
+win.geometry("750x750")
 win.title("Wordle Solver")
 
-
-label=Label(win, text="", font=("Times New Roman", 12))
+label = Label(win, text="", font=("Times New Roman", 12), fg="red")
 label.pack()
-label2=Label(win, text="", font=("Times New Roman", 12))
-label2.pack()
 
 
 global entry
-entry= Entry(win, width= 40)
+entry = Entry(win, width=40)
 entry.focus_set()
 entry.pack()
 
@@ -124,9 +154,14 @@ ttk.Button(win, text="Remove Word", width=40, command=remove_word).pack(pady=5)
 ttk.Button(win, text="Reset", width=40, command=reset_words).pack(pady=5)
 ttk.Button(win, text="Suggest Word", width=40, command=suggestions).pack(pady=5)
 
-global st
-st = ScrolledText(win, width=50,  height=10)
-st.pack(fill=tkinter.BOTH, side=tkinter.LEFT, expand=True)
-st.configure(state="disabled")
+global t
+S = tkinter.Scrollbar(win)
+T = tkinter.Text(win, height=4, width=100)
+S.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+T.pack(side=tkinter.LEFT, fill=tkinter.Y)
+S.config(command=T.yview)
+T.config(yscrollcommand=S.set)
 
+quote = "Posible words will appear here!"
+T.insert(tkinter.END, quote)
 win.mainloop()
